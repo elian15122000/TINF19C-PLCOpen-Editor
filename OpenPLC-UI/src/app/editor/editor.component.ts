9ac014c64842004cca41bf55c4c9895062c53d60
FranziKo <franzi.kopp@gmx.de>
2021-04-01 13:53:50 +0200

    Merge XML parser versions

--- OpenPLC-UI/src/app/editor/editor.component.ts
+++ OpenPLC-UI/src/app/editor/editor.component.ts
@@ -12,6 +12,20 @@ import {LdContact} from '../models/ldObjects/ldContact';
 import {LdCoil} from '../models/ldObjects/ldCoil';
 import {LdLeftPowerRail} from '../models/ldObjects/ldLeftPowerRail';
 import {LdRightPowerRail} from '../models/ldObjects/ldRightPowerRail';
+import {CommonActionBlock} from '../models/commonObjects/commonActionBlock';
+import {CommonComment} from '../models/commonObjects/commonComment';
+import {CommonConnector} from '../models/commonObjects/commonConnector';
+import {CommonContinuation} from '../models/commonObjects/commonContinuation';
+import {CommonError} from '../models/commonObjects/commonError';
+import {CommonVendorElement} from '../models/commonObjects/commonVendorElement';
+import {SfcJumpStep} from '../models/sfcObjects/sfcJumpStep';
+import {SfcMacroStep} from '../models/sfcObjects/sfcMacroStep';
+import {SfcSelectionConvergence} from '../models/sfcObjects/sfcSelectionConvergence';
+import {SfcSelectionDivergence} from '../models/sfcObjects/sfcSelectionDivergence';
+import {SfcSimultaneousConvergence} from '../models/sfcObjects/sfcSimultaneousConvergence';
+import {SfcSimultaneousDivergence} from '../models/sfcObjects/sfcSimultaneousDivergence';
+import {SfcStep} from '../models/sfcObjects/sfcStep';
+import {SfcTransition} from '../models/sfcObjects/sfcTransition';
 
 
 @Component({
@@ -41,6 +55,20 @@ export class EditorComponent implements OnInit {
       const coilList: LdCoil[] = [];
       const leftPowerRailList: LdLeftPowerRail[] = [];
       const rightPowerRailList: LdRightPowerRail[] = [];
+      const actionBlockList: CommonActionBlock[] = [];
+      const commentList: CommonComment[] = [];
+      const connectorList: CommonConnector[] = [];
+      const continuationList: CommonContinuation[] = [];
+      const errorList: CommonError[] = [];
+      const vendorElementList: CommonVendorElement[] = [];
+      const jumpStepList: SfcJumpStep[] = [];
+      const macroStepList: SfcMacroStep[] = [];
+      const selConvergenceList: SfcSelectionConvergence[] = [];
+      const selDivergenceList: SfcSelectionDivergence[] = [];
+      const simConvergenceList: SfcSimultaneousConvergence[] = [];
+      const simDivergenceList: SfcSimultaneousDivergence[] = [];
+      const stepList: SfcStep[] = [];
+      const transitionList: SfcTransition[] = [];
 
 
       for (const inVariable of pou.getElementsByTagName('inVariable')){
@@ -76,19 +104,48 @@ export class EditorComponent implements OnInit {
       for (const coil of pou.getElementsByTagName('coil')){
         coilList.push(new LdCoil(coil));
       }
-
-
-      console.log(inVariableList);
-      console.log(outVariableList);
-      console.log(inOutVariableList);
-      console.log(labelList);
-      console.log(returnList);
-      console.log(jumpList);
-      console.log(blockList);
-      console.log(contactList);
-      console.log(coilList);
-      console.log(leftPowerRailList);
-      console.log(rightPowerRailList);
+      for (const actionBlock of pou.getElementsByTagName('actionBlock')){
+        actionBlockList.push(new CommonActionBlock(actionBlock));
+      }
+      for (const comment of pou.getElementsByTagName('comment')){
+        commentList.push(new CommonComment(comment));
+      }
+      for (const connector of pou.getElementsByTagName('connector')){
+          connectorList.push(new CommonConnector(connector));
+      }
+      for (const continuation of pou.getElementsByTagName('continuation')){
+        continuationList.push(new CommonContinuation(continuation));
+      }
+      for (const error of pou.getElementsByTagName('error')){
+        errorList.push(new CommonError(error));
+      }
+      for (const vendorElement of pou.getElementsByTagName('vendorElement')){
+        vendorElementList.push(new CommonVendorElement(vendorElement));
+      }
+      for (const jumpStep of pou.getElementsByTagName('jumpStep')){
+        jumpStepList.push(new SfcJumpStep(jumpStep));
+      }
+      for (const macroStep of pou.getElementsByTagName('macroStep')){
+        macroStepList.push(new SfcMacroStep(macroStep));
+      }
+      for (const selectionConvergence of pou.getElementsByTagName('selectionConvergence')){
+        selConvergenceList.push(new SfcSelectionConvergence(selectionConvergence));
+      }
+      for (const selectionDivergence of pou.getElementsByTagName('selectionDivergence')){
+        selDivergenceList.push(new SfcSelectionDivergence(selectionDivergence));
+      }
+      for (const simultaneousConvergence of pou.getElementsByTagName('simultaneousConvergence')){
+        simConvergenceList.push(new SfcSimultaneousConvergence(simultaneousConvergence));
+      }
+      for (const simultaneousDivergence of pou.getElementsByTagName('simultaneousDivergence')){
+        simDivergenceList.push(new SfcSimultaneousDivergence(simultaneousDivergence));
+      }
+      for (const step of pou.getElementsByTagName('step')){
+        stepList.push(new SfcStep(step));
+      }
+      for (const transition of pou.getElementsByTagName('transition')){
+        transitionList.push(new SfcTransition(transition));
+      }
     }
   }
 
