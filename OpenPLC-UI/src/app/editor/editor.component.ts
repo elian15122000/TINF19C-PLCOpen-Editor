263933f6ff255f05a94313ba14a8940ac0537195
Mo-Official <inf19151@lehre.dhbw-stuttgart.de>
2021-05-13 14:16:39 +0200

    styling library

--- OpenPLC-UI/src/app/editor/editor.component.ts
+++ OpenPLC-UI/src/app/editor/editor.component.ts
@@ -46,12 +46,7 @@ export class EditorComponent implements OnInit {
 
   }
 
-
-
   ngOnInit(): void {
-    var x = window.scrollX;
-    var y = window.scrollY;
-    window.onscroll = function () { window.scrollTo(x, y); };
   }
 
 }
